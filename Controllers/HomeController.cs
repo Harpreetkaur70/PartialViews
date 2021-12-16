dc0a5990a85d2c700f5713235c55a9808012b979
Harpreet Kaur <harpreetkaur11379@gmail.com>
2021-12-16 22:29:04 +0530

    First Commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PartialViews.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PartialViews.Controllers
+{
+    public class HomeController : Controller
+    {
+        private List<Film> listFilms;
+
+        public HomeController()
+        {
+            listFilms = new List<Film>()
+            {
+                new Film(){ Title="Film A", Description = "Film A", Duration = "3 Hours", ID = 1 },
+                new Film(){ Title="Film B ", Description = "Film B", Duration = "2.5 Hours", ID = 2 },
+                new Film(){ Title="Film C", Description = "Film C", Duration = "2 Hours", ID = 3 }
+            };
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(listFilms);
+        }
+    }
+}
